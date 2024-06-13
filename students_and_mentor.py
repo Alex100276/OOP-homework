73105aacd7e31489ec1c7c37ba230e1337b465be
Александр <146880746+Alex100276@users.noreply.github.com>
2024-06-13 22:01:31 +0300

    task2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,22 +1,15 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-
-    def __str__(self):
-        res = f'Студент: \nИмя: {self.name}\nФамилия: {self.surname}\nПол: {self.gender}\nЗаконченные ' \
-              f'курсы: {self.finished_courses}\nКурсы в процессе ' \
-              f'изучения: {self.courses_in_progress}\nОценки: {self.grades}'
-        return res
+        self.average_rating = []
 
     def rate_h(self, lecturer, course, grade):
         """Выставления оценки лектору студентом, по курсу у этого студента
         Принимает на вход переменные rate_h(self, lecturer, course, grade)"""
-
         if isinstance(lecturer,
                       Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -25,6 +18,26 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+
+        grades_count = 0
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+
+        res = f'Студент: \nИмя: {self.name}\nФамилия: {self.surname}\n' \
+            f'Законченные курсы: {self.finished_courses}\n' \
+            f'Курсы в процессе изучения: {self.courses_in_progress}\n' \
+            f'Средняя оценка за домашнее задание: {self.average_rating}'
+        return res
+
+    def __lt__(self, other):
+        """Реализует сравнение через операторы '<, >' студентов между собой по средней оценке за домашние задания"""
+        if not isinstance(other, Student):
+            print('Такое сравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
 
 
 class Mentor:  # преподаватели
@@ -41,11 +54,25 @@ class Lecturer(Mentor):  # лекторы
         self.surname = surname
         self.courses_attached = []  # курсы
         self.grades = {}
+        self.average_rating = []  # средняя оценка за лекции
 
     def __str__(self):
-        res = f'Лектор: \nИмя: {self.name}\nФамилия: {self.surname}\nПрикрепленные ' \
-              f'курсы: {self.courses_attached}\nОценки: {self.grades}'
+        """Возвращает характеристики экземпляра класса вида"""
+        grades_count = 0
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+
+        res = f'Лектор: \nИмя: {self.name}\nФамилия: {self.surname}\n' \
+            f'Средняя оценка за лекции: {self.average_rating}'
         return res
+
+    def __lt__(self, other):
+        """Реализует сравнение через операторы '<, >' лекторов между собой по средней оценке за лекции"""
+        if not isinstance(other, Lecturer):
+            print('Такое сравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
 
 
 class Reviewer(Mentor):  # эксперты, проверяющие домашние задания
@@ -56,13 +83,11 @@ class Reviewer(Mentor):  # эксперты, проверяющие домашн
         self.courses_attached = []  # курсы
 
     def __str__(self):
-        res = f'Эксперт: \nИмя: {self.name}\nФамилия: {self.surname}\nПрикрепленные ' \
-              f'курсы: {self.courses_attached}'
+        res = f'Эксперт: \nИмя: {self.name}\nФамилия: {self.surname}'
         return res
 
     def rate_hw(self, student, course, grade):
-        """Реализует возможность выставления оценки студенту,
-        или возвращает ошибку.
+        """Возможность выставления оценки студенту
         Принимает на вход переменные rate_hw(self, student, course, grade)"""
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -74,12 +99,12 @@ class Reviewer(Mentor):  # эксперты, проверяющие домашн
 
 
 # студенты
-student1 = Student('Ruoy', 'Eman', 'man')
+student1 = Student('Ruoy', 'Eman')
 student1.courses_in_progress += ['Python']
 student1.courses_in_progress += ['Git']
 student1.finished_courses += ['Введение в программирование']
 
-student2 = Student('Иванов', 'Иван', 'man')
+student2 = Student('Иванов', 'Иван')
 student2.courses_in_progress += ['Python']
 student2.courses_in_progress += ['Git']
 student2.finished_courses += ['Введение в программирование']
@@ -101,19 +126,14 @@ reviewer1.rate_hw(student2, 'Python', 9)
 reviewer1.rate_hw(student2, 'Cit', 9)
 
 # лекторы
+
 lecturer1 = Lecturer('Яков', 'Яковлев')
 lecturer1.courses_attached += ['Python']
 lecturer1.courses_attached += ['Git']
 
-student1.rate_h(lecturer1, 'Python', 10)
-student1.rate_h(lecturer1, 'Git', 10)
-
 lecturer2 = Lecturer('Филип', 'Филипов')
 lecturer2.courses_attached += ['Python']
 lecturer2.courses_attached += ['Git']
-
-student1.rate_h(lecturer1, 'Python', 9)
-student1.rate_h(lecturer1, 'Git', 10)
 
 # оценки лекторам
 student1.rate_h(lecturer1, 'Python', 10)
@@ -129,4 +149,11 @@ print(lecturer1)
 print(lecturer2)
 
 print(reviewer1)
+
 print(reviewer2)
+
+print(f'Результат сравнения студентов (по средним оценкам за ДЗ): '
+      f'{student1.name} {student1.surname} < {student2.name} {student2.surname} = {student1 > student2}')
+
+print(f'Результат сравнения лекторов (по средним оценкам за лекции): '
+      f'{lecturer1.name} {lecturer1.surname} < {lecturer2.name} {lecturer2.surname} = {lecturer1 > lecturer2}')
